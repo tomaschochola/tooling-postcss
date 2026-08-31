5db225f6fc08bff8477303b5a1de534f05ed9d6e
Tomáš Chochola <tomaschochola@tomaschochola.cz>
2026-08-31 13:56:12 +0200

    chore: align repository tooling

--- src/index.js
+++ src/index.js
@@ -15,34 +15,34 @@ import postcssPresetEnv from 'postcss-preset-env';
 const pluginFactory = ([factory]) => factory;
 
 export class PostCSSConfigBuilder {
-  #config;
+    #config;
 
-  constructor() {
-    this.#config = {
-      plugins: [],
-    };
-  }
+    constructor() {
+        this.#config = {
+            plugins: [],
+        };
+    }
 
-  #replaceConfig(config) {
-    this.#config = { ...config };
+    #replaceConfig(config) {
+        this.#config = { ...config };
 
-    return this;
-  }
+        return this;
+    }
 
-  addPresetEnvPlugin(options = {}) {
-    const plugin = [postcssPresetEnv, { ...options }];
-    const existingIndex = this.#config.plugins.findIndex((item) => pluginFactory(item) === postcssPresetEnv);
+    addPresetEnvPlugin(options = {}) {
+        const plugin = [postcssPresetEnv, { ...options }];
+        const existingIndex = this.#config.plugins.findIndex((item) => pluginFactory(item) === postcssPresetEnv);
 
-    return this.#replaceConfig({
-      ...this.#config,
-      plugins: existingIndex === -1 ? [...this.#config.plugins, plugin] : this.#config.plugins.map((item, index) => (index === existingIndex ? plugin : item)),
-    });
-  }
+        return this.#replaceConfig({
+            ...this.#config,
+            plugins: existingIndex === -1 ? [...this.#config.plugins, plugin] : this.#config.plugins.map((item, index) => (index === existingIndex ? plugin : item)),
+        });
+    }
 
-  toConfig() {
-    return {
-      ...this.#config,
-      plugins: this.#config.plugins.map(([factory, options]) => factory({ ...options })),
-    };
-  }
+    toConfig() {
+        return {
+            ...this.#config,
+            plugins: this.#config.plugins.map(([factory, options]) => factory({ ...options })),
+        };
+    }
 }
